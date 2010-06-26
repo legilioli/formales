01e4a6f51068ea7414b63edec6305865ae89b125
Leandro Gilioli <legilioli@gmail.com>
2010-06-26 18:53:06 -0300

    procesamiento de condicionales (ifs)

--- interpretes/c/c.lisp
+++ interpretes/c/c.lisp
@@ -3,6 +3,18 @@
 	nil
 )
 
+(defun evaluar (prg mem)
+	prg
+)
+
+(defun esasignacion (expr)
+	nil
+)
+
+(defun asignacion (expr memoria)
+	nil
+)
+
 (defun asociar (var valor memoria)
 	(if (null memoria) (list var valor)
 		(if (equal (caar memoria) var) (cons (list var valor) (cdr memoria))
@@ -11,7 +23,7 @@
 	)
 )
 
-(defun run (prg ent &optional (salida nil))
+(defun run (prg ent &optional (mem nil))
 	(if (null prg) nil
 		(if (eq (caar prg) 'int) (run (cdr prg) ent (agregarmem (cdar prg) mem))
 			(if (eq (caar prg) 'main) (ejecutar (cadar prg) ent mem) 'ERROR)	
@@ -27,10 +39,31 @@
 	(if (null prg) salida
 		(cond
 			( (esfuncion prg 'scanf) (ejecutar (cdr prg)(cdr ent) (asociar (cadar prg)(car ent) mem) salida))
-			( (esfuncion prg 'printf) (ejecutar (cdr prg) ent mem (cons (eval (cadar prg) mem)) ) )
-			(t nil)	
+			( (esfuncion prg 'printf) (ejecutar (cdr prg) ent mem (cons (evaluar (cadar prg) mem) salida) ) )
+			( (esasignacion (car prg)) (ejecutar (cdr prg) ent (asignacion (car prg) mem) salida ) )
+			( (esfuncion prg 'if) 
+				(if (not (equal (evaluar (cadar prg) mem) 0)) 
+					(ejecutar (append (list (nth 2 (car prg))) (cdr prg)) ent mem salida); ejecuto if
+					(if (equal (length (car prg)) 5) (ejecutar (append (list (nth 4 (car prg))) (cdr prg)) ent mem salida)
+						(ejecutar (cdr prg) ent mem salia)
+					) ;ejecuto else
+				))
+	
+			(t (list 'syntax_error  prg ))	
 		)
 	)
 )
 
+(setq code '(
+	(int j=10)
+	(main( 
+		(printf "hola_moncho")
+		(if 0 (printf "cond_true") else (printf "cond_false"))	
+		)
+	)
+))
 
+;(trace ejecutar)
+;(trace evaluar)
+(run code nil)
+;(run '((int i j)(main () )) nil)
