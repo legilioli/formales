93b87d92d131f353771eee639f19301b4c606b7c
Leandro Gilioli <legilioli@gmail.com>
2010-06-26 19:12:50 -0300

    agrego procesamient de whiles

--- interpretes/c/c.lisp
+++ interpretes/c/c.lisp
@@ -44,6 +44,13 @@
 	)
 )
 
+(defun procesar_while (prg ent mem salida)
+	(if (not (equal (evaluar (nth 2 (car prg))) 0))
+		(ejecutar (append (list (nth 2 (car prg)) ) prg) ent mem salida)
+		(ejecutar (cdr prg) ent mem salida)
+	)
+)
+
 (defun ejecutar (prg ent mem &optional (salida nil))
 	(if (null prg) salida
 		(cond
@@ -51,7 +58,7 @@
 			( (esfuncion prg 'printf) (ejecutar (cdr prg) ent mem (cons (evaluar (cadar prg) mem) salida) ) )
 			( (esasignacion (car prg)) (ejecutar (cdr prg) ent (asignacion (car prg) mem) salida ) )
 			( (esfuncion prg 'if) (procesar_if prg ent mem salida ))
-			( (esfuncion prg 'while) (nil))
+			( (esfuncion prg 'while)(procesar_while prg ent mem salida )) 
 			(t (list 'syntax_error  prg ))	
 		)
 	)
@@ -61,6 +68,7 @@
 	(int j=10)
 	(main( 
 		(printf "hola_moncho")
+		(if 0 (printf "cond_true") else (printf "cond_false"))	
 		(if 0 (printf "cond_true") else (printf "cond_false"))	
 		)
 	)
